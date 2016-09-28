eaca3327b8e406769271cee9ff470489118b8dfd
Alexey Verkeenko <averkeenko@grossum.com>
2016-09-28 18:28:41 +0300

    Update component

--- app/app.component.ts
+++ app/app.component.ts
@@ -70,6 +70,15 @@ const HEROES:Hero[] = [
           }
         `],
     template: `
+        <h1>{{title}}</h1>
+        <h2>My Heroes</h2>
+        <ul class="heroes">
+          <li *ngFor="let hero of heroes"
+            [class.selected]="hero === selectedHero"
+            (click)="onSelect(hero)">
+            <span class="badge">{{hero.id}}</span> {{hero.name}}
+          </li>
+        </ul>
         <div *ngIf="selectedHero">
           <h2>{{selectedHero.name}} details!</h2>
           <div><label>id: </label>{{selectedHero.id}}</div>
@@ -78,13 +87,6 @@ const HEROES:Hero[] = [
             <input [(ngModel)]="selectedHero.name" placeholder="name"/>
           </div>
         </div>
-        <ul class="heroes">
-          <li *ngFor="let hero of heroes"
-            [class.selected]="hero === selectedHero"
-            (click)="onSelect(hero)">
-            <span class="badge">{{hero.id}}</span> {{hero.name}}
-          </li>
-        </ul>
     `
 })
 
