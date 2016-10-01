241b94b6d940a0dee5307d9c813be9b73ca5bff2
TeodorSolvic <trip.of.neo@gmail.com>
2016-10-01 13:30:34 +0300

    Add routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,31 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-
-import { AppComponent }   from './app.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { AppComponent }        from './app.component';
+import {DashboardComponent} from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+import {routing} from './app.routing';
 
 @NgModule({
-    imports:      [
+    imports: [
         BrowserModule,
-        FormsModule
+        FormsModule,
+        routing
     ],
     declarations: [
         AppComponent,
-        HeroDetailComponent
+        DashboardComponent,
+        HeroDetailComponent,
+        HeroesComponent
     ],
-    bootstrap:    [ AppComponent ]
+    providers: [
+        HeroService
+    ],
+    bootstrap: [AppComponent]
 })
 
-export class AppModule { }
+export class AppModule {
+
+}
