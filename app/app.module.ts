816ece5f413846296b12c9263cc3a2bea92de577
TeodorSolvic <trip.of.neo@gmail.com>
2016-10-01 23:28:33 +0300

    Work on http part

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent }        from './app.component';
-import {DashboardComponent} from './dashboard.component';
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
-import {routing} from './app.routing';
+import { routing } from './app.routing';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
         routing
     ],
     declarations: [
--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { Router } from '@angular/router';
 
 @Component({
     moduleId: module.id,
     selector: 'my-dashboard',
+    styleUrls: [ 'dashboard.component.css' ],
     templateUrl: 'dashboard.component.html'
 })
 
@@ -13,12 +14,18 @@ export class DashboardComponent implements OnInit {
 
     heroes: Hero[] = [];
 
-    constructor(private heroService: HeroService) { }
+    constructor(
+        private router: Router,
+        private heroService: HeroService) {
+    }
 
     ngOnInit(): void {
         this.heroService.getHeroes()
             .then(heroes => this.heroes = heroes.slice(1, 5));
     }
 
-    gotoDetail(hero: Hero): void { /* not implemented yet */}
+    gotoDetail(hero: Hero): void {
+        let link = ['/detail', hero.id];
+        this.router.navigate(link);
+    }
 }
